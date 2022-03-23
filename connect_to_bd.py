3b506548fcad0858f10090588aab86b1204c5c94
69ccup <98023101+69ccup@users.noreply.github.com>
2022-03-23 14:21:06 +0300

    Add files via upload

--- connect_to_bd.py
+++ connect_to_bd.py
@@ -1,7 +1,32 @@
 from openpyxl import load_workbook
 
-bd = load_workbook('Stickers.xlsx')
-stickers_page = bd['stickers']
+
+def insert_sticker(keyword, sticker_id=None, reply_text=None):
+    row = stickers_page.max_row + 1
+    stickers_page.cell(row=row, column=1).value = keyword
+    stickers_page.cell(row=row, column=2).value = sticker_id
+    stickers_page.cell(row=row, column=3).value = reply_text
+    bd.save(database_filename)
+    stickers[keyword] = sticker_id
+    replies[keyword] = reply_text
+
+
+def in_database(user)-> bool:
+    '''
+    Return True if user_id is already in database
+    '''
+    users_page = bd['Users']
+    for row in range(2, users_page.max_row + 1):
+        if user == users_page.cell(row=row, column=1).value:
+            return True
+        return False
+
+
+
+
+database_filename = 'DTbot.xlsx'
+bd = load_workbook(database_filename)
+stickers_page = bd['Stickers']
 
 stickers = {}
 replies = {}
@@ -13,5 +38,7 @@ for row in range(2, stickers_page.max_row + 1):
     stickers[keyword] = sticker_id
     replies[keyword] = reply_text
 
+
 if __name__ == '__main__':
     print(stickers)
+    insert_sticker('до свидания', reply_text='и вам не хворать')
--- yokai_bot.py
+++ yokai_bot.py
@@ -1,7 +1,7 @@
 from telegram import Update, ReplyKeyboardMarkup, ReplyKeyboardRemove
 from telegram.ext import Updater, MessageHandler, Filters, CallbackContext
 from key import TOKEN
-from connect_to_bd import stickers, replies
+from connect_to_bd import stickers, replies, insert_sticker, in_database
 
 
 def main():
@@ -13,11 +13,17 @@ def main():
     dispatcher = updater.dispatcher
 
     echo_handler = MessageHandler(Filters.all, do_echo)
-    text_handler = MessageHandler(Filters.text, say_smth)
     keyboard_handler = MessageHandler(Filters.text('Клавиатура, клавиатура'), keyboard)
     static_handler = MessageHandler(Filters.text('Статистика, статистика'), static)
+    sticker_handler = MessageHandler(Filters.sticker, reply_sticker)
+    say_smth_handler = MessageHandler(Filters.text, say_smth)
+    new_sticker_handler = MessageHandler(Filters.sticker, new_sticker)
+    text_handler = MessageHandler(Filters.text, new_keyword)
 
+    dispatcher.add_handler(new_sticker_handler)
     dispatcher.add_handler(text_handler)
+    dispatcher.add_handler(say_smth_handler)
+    dispatcher.add_handler(sticker_handler)
     dispatcher.add_handler(keyboard_handler)
     dispatcher.add_handler(static_handler)
     dispatcher.add_handler(echo_handler)
@@ -32,12 +38,7 @@ def do_echo(update: Update, context: CallbackContext) -> None:
     surname = update.message.from_user.last_name
     telegram_id = update.message.chat_id
     username = update.message.from_user.username
-    text = update.message.text if update.message.text else None
-    sticker = update.message.sticker if update.message.sticker else None
-    if sticker:
-        sticker_id = sticker.file_id
-        update.message.reply_sticker(sticker_id)
-        print(sticker, sticker_id)
+    text = update.message.text
     update.message.reply_text(text=f'{text}')
     print(username, ':', text)
 
@@ -60,7 +61,7 @@ def static(update: Update, context: CallbackContext) -> None:
 
 def keyboard(update: Update, context: CallbackContext) -> None:
     buttons = [
-        ['1', '2', '3'],
+        ['Добавить стикер'],
         ['Привет', 'Пока']
     ]
     keys = ReplyKeyboardMarkup(
@@ -77,14 +78,82 @@ def keyboard(update: Update, context: CallbackContext) -> None:
     )
 
 
+def reply_sticker(update: Update, context: CallbackContext) -> None:
+    sticker = update.message.sticker
+    if sticker:
+        sticker_id = sticker.file_id
+        update.message.reply_sticker(sticker_id)
+        print(sticker, sticker_id)
 
-def say_smth(update: Update, context: CallbackContext):
+
+def say_smth(update: Update, context: CallbackContext) -> None:
     name = update.message.from_user.first_name
     text = update.message.text
     for keyword in stickers:
         if keyword in text:
-            update.message.reply_sticker(stickers[keyword])
-            update.message.reply_text(replies[keyword].format(name))
+            if stickers[keyword]:
+                update.message.reply_sticker(stickers[keyword])
+            if replies[keyword]:
+                update.message.reply_text(replies[keyword])
+    else:
+        do_echo(update, context)
+
+
+def new_sticker(update: Update, context: CallbackContext) -> None:
+    sticker_id = update.message.sticker.file_id
+    for keyword in stickers:
+        if sticker_id == stickers[keyword]:
+            update.message.reply_text('у меня тоже такой есть')
+            update.message.reply_sticker(sticker_id)
+            break
+    else:
+        context.user_data['new_sticker'] = sticker_id
+        update.message.reply_text('введи ключевое слово')
+
+
+def new_keyword(update: Update, context: CallbackContext) -> None:
+    if 'new_sticker' not in context.user_data:
+        say_smth(update, context)
+    else:
+        keyword = update.message.text
+        sticker_id = context.user_data['new_sticker']
+        insert_sticker(keyword, sticker_id)
+        context.user_data.clear()
+
+
+def meet(update: Update, context: CallbackContext):
+    ''' start to add in database:
+        id
+        name
+        sex
+        grade
+    '''
+    telegram_id = update.message.from_user.id
+    if in_database(telegram_id):
+        pass
+    update.message.reply_text(
+        'Привет имя введи пжпжпж'
+    )
+    
+
+
+def ask_name(update: Update, context: CallbackContext):
+    pass
+
+
+def ask_sex(update: Update, context: CallbackContext):
+    pass
+
+
+def ask_grade(update: Update, context: CallbackContext):
+    pass
+
+
+def greet(update: Update, context: CallbackContext):
+    pass
+
+
+
 
 if __name__ == '__main__':
     main()
